cc73eb3516b7538f57681b2530a09aa97a5b6ee5
owaisnaeem-coding <0hussainnaeem786@gmail.com>
2025-11-23 16:44:54 +0500

    some changes

--- src/app.js
+++ src/app.js
@@ -33,17 +33,17 @@ function render() {
 --------------------------------------------- */
 function renderLogin() {
   $("app").innerHTML = `
-  <div class="bg-white p-6 rounded shadow">
-    <h2 class="text-xl font-bold mb-3 text-center">Login</h2>
+  <div class="bg-gray-950 p-6 rounded-2xl shadow border-[crimson] border-2 flex flex-col justify-center items-center">
+    <h2 class="text-2xl font-bold mb-3 text-center text-white">Login</h2>
 
-    <input id="email" class="w-full border p-2 mb-3" placeholder="Email">
-    <input id="pass" type="password" class="w-full border p-2 mb-3" placeholder="Password">
+    <input id="email" class="w-4/5 border p-2 mb-3 bg-[#0f172a] text-gray-300/50 rounded-lg" placeholder="Email">
+    <input id="pass" type="password" class="w-4/5 border p-2 mb-3 bg-[#0f172a] text-gray-300/50 rounded-lg" placeholder="Password">
 
-    <button onclick="login()" class="w-full bg-blue-600 text-white p-2 rounded">Login</button>
+    <button onclick="login()" class="w-4/5 bg-[crimson] text-white p-2 rounded-lg">Login</button>
 
-    <p class="mt-4 text-sm text-center">
+    <p class="mt-4 text-sm text-center text-white">
       No account?
-      <button onclick="renderSignup()" class="text-blue-500">Sign up</button>
+      <button onclick="renderSignup()" class="text-[crimson]">Sign up</button>
     </p>
   </div>`;
 }
@@ -53,17 +53,17 @@ function renderLogin() {
 --------------------------------------------- */
 function renderSignup() {
   $("app").innerHTML = `
-  <div class="bg-white p-6 rounded shadow">
-    <h2 class="text-xl font-bold mb-3 text-center">Sign Up</h2>
+  <div class="bg-gray-950 p-6 rounded-2xl shadow border-[crimson] border-2 flex flex-col justify-center items-center">
+    <h2 class="text-2xl font-bold mb-3 text-center text-white">Sign Up</h2>
+    
+    <input id="regEmail" type="email" class="w-4/5 border p-2 mb-3 bg-[#0f172a] text-gray-300/50 rounded-lg focus:outline-none focus:border-[crimson]" placeholder="Email">
+    <input id="regPass" type="password" class="w-4/5 border p-2 mb-3 bg-[#0f172a] text-gray-300/50 rounded-lg focus:outline-none focus:border-[crimson]" placeholder="Password">
 
-    <input id="regEmail" class="w-full border p-2 mb-3" placeholder="Email">
-    <input id="regPass" type="password" class="w-full border p-2 mb-3" placeholder="Password">
+    <button onclick="signup()" class="w-4/5 bg-[crimson] text-white p-2 rounded-lg">Create Account</button>
 
-    <button onclick="signup()" class="w-full bg-green-600 text-white p-2 rounded">Create Account</button>
-
-    <p class="mt-4 text-sm text-center">
+    <p class="mt-4 text-sm text-center text-white">
       Already registered?
-      <button onclick="renderLogin()" class="text-blue-500">Login</button>
+      <button onclick="renderLogin()" class="text-[crimson] ">Login</button>
     </p>
   </div>`;
 }
@@ -109,19 +109,19 @@ function logout() {
 --------------------------------------------- */
 function renderHome() {
   $("app").innerHTML = `
-  <div class="flex justify-between items-center mb-4">
-    <h2 class="text-xl font-bold">Welcome ${loggedUser}</h2>
-    <button onclick="logout()" class="text-red-500">Logout</button>
+  <div class="flex justify-between items-center mb-4 ">
+    <h2 class="text-2xl font-bold text-white">Welcome ${loggedUser}</h2>
+    <button onclick="logout()" class="text-white bg-[crimson] px-4 py-2 rounded-lg">Logout</button>
   </div>
 
   <!-- Create Post -->
-  <div class="bg-white p-4 rounded shadow mb-4">
-    <textarea id="postText" class="w-full border p-2" rows="3" placeholder="Write something..."></textarea>
-    <input id="postImage" type="file" class="mt-2" accept="image/*">
-    <button onclick="addPost()" class="w-full bg-blue-600 text-white p-2 rounded mt-2">Post</button>
+  <div class="bg-gray-950 p-4 rounded-2xl shadow-white shadow-2xl mb-4 flex flex-col justify-center items-center">
+    <textarea id="postText" class="w-4/5 bg-[#0f172a] border-[crimson] border-2 rounded-lg p-2" rows="3" placeholder="Write something..."></textarea>
+    <input id="postImage" type="file" class="mt-2 text-[crimson]" accept="image/*">
+    <button onclick="addPost()" class="w-4/5 bg-[crimson] text-white p-2 rounded-lg">Post</button>
   </div>
 
-  <h3 class="font-bold mb-2">Posts</h3>
+  <h3 class="font-bold mb-2 text-white text-2xl">Posts</h3>
   <div id="posts"></div>
   `;
 
@@ -172,12 +172,12 @@ function renderPosts() {
 
   posts.slice().reverse().forEach((p) => {
     container.innerHTML += `
-    <div class="bg-white p-4 rounded shadow mb-4">
-      <p class="mb-2">${p.text}</p>
+    <div class="bg-gray-950 p-4 rounded-2xl shadow-white shadow-2xl mb-4 flex flex-col justify-center items-center border-[crimson] border-2">
+      <p class="mb-2 text-white">${p.text}</p>
       ${p.image ? `<img src="${p.image}" class="w-full mb-2 rounded">` : ""}
       
       <div class="flex gap-3 text-sm">
-        <button onclick="likePost('${p.id}')" class="text-blue-600">❤️ ${p.likes}</button>
+        <button onclick="likePost('${p.id}')" class="text-[crimson]">❤️ ${p.likes}</button>
 
         ${p.user === loggedUser ?
          `<button class="text-green-600" onclick="editPost('${p.id}')">Edit</button>
@@ -190,7 +190,7 @@ function renderPosts() {
         ${p.comments.map(c => `<p class="text-sm">💬 <b>${c.user}</b>: ${c.text}</p>`).join("")}
       </div>
 
-      <input id="c-${p.id}" class="w-full border p-1 text-sm mt-2" placeholder="Comment...">
+      <input id="c-${p.id}" class="w-full rounded-md p-1 text-sm mt-2 border-[crimson]/50 border-2 bg-[#0f172a]" placeholder="Comment...">
       <button onclick="addComment('${p.id}')" class="text-sm text-blue-600 mt-1">Send</button>
     </div>
     `;
